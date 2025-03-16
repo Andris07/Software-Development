337ebd87ac3399b99cc9f8ca5ba9feafb9dba8c3
Andris <laczkovicsandris07.education@gmail.com>
2025-03-16 14:21:40 +0100

    Elmélet - Példakódok feltöltése
    Március 15.-ig felrakott összes példakód solution mappája

---
+++ 2024-2025/Asztali alkalmazások fejlesztése/Elmélet - Példakódok/Kor_PL/Kor_PL/Kor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kor_PL
+{
+    public class Kor
+    {
+		private double r;
+
+		public double Sugar
+		{
+			get { return r; }
+			set 
+			{ 
+				if (value > 0) r = value;
+				else r = 1;
+			}
+		}
+
+		/// <summary>
+		/// 1 sugarú kör létrehozása
+		/// </summary>
+        public Kor()
+        {
+			r = 1;
+        }
+
+		/// <summary>
+		/// A paraméterben megadott sugarú kör létrehozása.
+		/// Negatív vagy 0 értékű paraméter esetén 1 sugarú kört hoz létre.
+		/// </summary>
+		/// <param name="r">A sugár nagysága</param>
+        public Kor(double r)
+        {
+           // this.r = r;
+		   Sugar = r;
+        }
+
+		public double Kerulet()
+		{
+			return 2 * r * Math.PI;
+		}
+
+		public double Terulet() => r * r * Math.PI;
+
+		public double FelKerulet
+		{
+			get 
+			{
+				return r * Math.PI;
+			}
+		}
+
+		public double FelKerulet2 => r * Math.PI;
+
+		public string Info()
+		{
+			return $"A kör sugara: {Sugar} kerülete: {Kerulet()} területe: {Terulet()}";
+		}
+
+    }
+}
---
+++ 2024-2025/Asztali alkalmazások fejlesztése/Elmélet - Példakódok/Kor_PL/Kor_PL/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Kor_PL;
+
+Kor kor = new Kor();
+Console.WriteLine($"A kör sugara: {kor.Sugar} kerülete: {kor.Kerulet()} területe: {kor.Terulet()}");
+Console.WriteLine(kor.Info());
+
+kor.Sugar = 0;
+Console.WriteLine("0-ra állított sugár");
+Console.WriteLine(kor.Info());
+
+kor.Sugar = -6;
+Console.WriteLine("-6-ra állított sugár");
+Console.WriteLine(kor.Info());
+
+kor.Sugar = 6;
+Console.WriteLine("6-ra állított sugár");
+Console.WriteLine(kor.Info());
+
+Kor kor2 = new Kor(0);
+Console.WriteLine("0-ra állított sugár");
+Console.WriteLine(kor2.Info());
+
+kor2 = new Kor(-6);
+Console.WriteLine("-6-ra állított sugár");
+Console.WriteLine(kor2.Info());
+
+kor2 = new Kor(6);
+Console.WriteLine("6-ra állított sugár");
+Console.WriteLine(kor2.Info());
