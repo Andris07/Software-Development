b33fc499a81f1fff605d0807bc059a7989a4a3e4
Andris <laczkovicsandris07.education@gmail.com>
2025-03-30 21:29:17 +0200

    Asztali 03.11_órai feltöltése (kibővítve)

---
+++ 2024-2025/Asztali alkalmazások fejlesztése/Gyakorlat/03.11_órai/Autok/Autok/Auto.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autok
+{
+    internal class Auto
+    {
+        List<int> sebessegek = new();
+
+        public string Tipus { get; init; }
+        public string Vezeto { get; set; }
+        public int MaxSebesseg { get; init; }
+        private double AtlagSebesseg
+        {
+            get
+            {
+                if (sebessegek.Count ==0)
+                    return 0;
+                return sebessegek.Average();
+            }
+        }
+        
+
+        public Auto()
+        {
+            Tipus = "Ismeretlen";
+            Vezeto = string.Empty;
+            MaxSebesseg = 100;
+        }
+
+        public Auto(string tipus, string vezeto, int maximalisSebesseg)
+        {
+            Tipus = tipus;
+            Vezeto = vezeto;
+            if (maximalisSebesseg < 100) 
+                MaxSebesseg = 100;
+            else MaxSebesseg = maximalisSebesseg;
+        }
+
+        public int SebessegMeres()
+        {
+            if (Vezeto==string.Empty) return 0;
+            int sebesseg = Random.Shared.Next(MaxSebesseg/4, MaxSebesseg);
+            sebessegek.Add(sebesseg);
+            return sebesseg;
+        }
+
+        public string Log()
+        {
+            return $"Sofőr: {Vezeto} átlagsebesség: {AtlagSebesseg}";
+        }
+
+        public int Osszehasonlitas(Auto auto)
+        {
+            if (AtlagSebesseg > auto.AtlagSebesseg)
+                return 1;
+            else if (auto.AtlagSebesseg > AtlagSebesseg)
+                return -1;
+            else return 0;
+        }
+
+    }
+}
---
+++ 2024-2025/Asztali alkalmazások fejlesztése/Gyakorlat/03.11_órai/Autok/Autok/Program.cs
@@ -0,0 +1,21 @@
+﻿using Autok;
+
+Auto auto1 = new();
+Auto auto2 = new("Ferrari", "Tigris", 600);
+
+Console.WriteLine(auto1.Log());
+auto1.SebessegMeres();
+Console.WriteLine(auto1.Log());
+auto1.Vezeto = "Leopárd";
+Console.WriteLine(auto1.Log());
+for (int i = 0; i < 10; i++)
+    auto1.SebessegMeres();
+Console.WriteLine(auto1.Log());
+
+Console.WriteLine(auto2.Log());
+for (int i = 0; i < 10; i++)
+    auto2.SebessegMeres();
+Console.WriteLine(auto2.Log());
+
+Console.WriteLine(auto1.Osszehasonlitas(auto1));
+Console.WriteLine(auto1.Osszehasonlitas(auto2));
