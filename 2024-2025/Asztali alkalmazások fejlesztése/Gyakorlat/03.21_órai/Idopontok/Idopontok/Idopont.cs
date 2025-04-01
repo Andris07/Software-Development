8d1c464a8a7512374733cd9f092f0d8558997da8
Andris <laczkovicsandris07.education@gmail.com>
2025-04-01 16:38:46 +0200

    Asztali 03.21_órai feltöltése

---
+++ 2024-2025/Asztali alkalmazások fejlesztése/Gyakorlat/03.21_órai/Idopontok/Idopontok/Idopont.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idopontok
+{
+    internal class Idopont
+    {
+        private int ora;
+        private int perc;
+        private int masodPerc;
+
+        public int Ora
+        {
+            get
+            {
+                return ora;
+            }
+            set
+            {
+                if (value < 0 || value > 23)
+                {
+                    ora = 0;
+                }
+                else
+                {
+                    ora = value;
+                }
+            }
+        }
+
+        public int Perc
+        {
+            get
+            {
+                return perc;
+            }
+            set
+            {
+                if (value < 0 || value > 59)
+                {
+                    perc = 0;
+                }
+                else
+                {
+                    perc = value;
+                }
+            }
+        }
+
+        public int MasodPerc
+        {
+            get
+            {
+                return masodPerc;
+            }
+            set
+            {
+                if (value < 0 || value > 59)
+                {
+                    masodPerc = 0;
+                }
+                else
+                {
+                    masodPerc = value;
+                }
+            }
+        }
+
+        public bool Delelott
+        {
+            get
+            {
+                if (ora < 12)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public Idopont()
+        {
+            Random r = new Random();
+            ora = r.Next(0, 24);
+            perc = r.Next(0, 60);
+            masodPerc = r.Next(0, 60);
+        }
+
+
+        public Idopont(int ora, int perc, int masodPerc)
+        {
+            Ora = ora;
+            Perc = perc;
+            MasodPerc = masodPerc;
+        }
+
+        public int HanyPercEddig()
+        {
+            return ora * 60 + perc;
+        }
+
+        public int HanyMasodPercEddig()
+        {
+            return ora * 60 * 60 + masodPerc;
+        }
+
+        public string Megjelenites()
+        {
+            return $"{ora}:{perc}:{masodPerc} az idő, vagyis a nap kezdetétől {HanyPercEddig()} perc, valamint {HanyMasodPercEddig()} másodperc telt el eddig";
+        }
+    }
+}
---
+++ 2024-2025/Asztali alkalmazások fejlesztése/Gyakorlat/03.21_órai/Idopontok/Idopontok/Program.cs
@@ -0,0 +1,40 @@
+﻿using Idopontok;
+
+// teszt1
+Console.WriteLine("teszt1");
+Idopont teszt1 = new Idopont();
+
+// Értékek kiírása
+Console.WriteLine($"{teszt1.Ora} óra");
+Console.WriteLine($"{teszt1.Perc} perc");
+Console.WriteLine($"{teszt1.MasodPerc} másodperc");
+Console.WriteLine();
+
+// Metódusok ellenőrzése
+Console.WriteLine($"Délelőtt van-e? {teszt1.Delelott}");
+Console.WriteLine($"{teszt1.HanyPercEddig()} perc telt el eddig");
+Console.WriteLine($"{teszt1.HanyMasodPercEddig()} másodperc telt el eddig");
+Console.WriteLine();
+
+// Végső megjelenítés
+Console.WriteLine(teszt1.Megjelenites());
+Console.WriteLine();
+
+// teszt2
+Console.WriteLine("teszt2");
+Idopont teszt2 = new Idopont(32, 72, 24);
+
+// Értékek kiírása
+Console.WriteLine($"{teszt2.Ora} óra");
+Console.WriteLine($"{teszt2.Perc} perc");
+Console.WriteLine($"{teszt2.MasodPerc} másodperc");
+Console.WriteLine();
+
+// Metódusok ellenőrzése
+Console.WriteLine($"Délelőtt van-e? {teszt1.Delelott}");
+Console.WriteLine($"{teszt1.HanyPercEddig()} perc telt el eddig");
+Console.WriteLine($"{teszt1.HanyMasodPercEddig()} másodperc telt el eddig");
+Console.WriteLine();
+
+// Végső megjelenítés
+Console.WriteLine(teszt2.Megjelenites());
