fa2b453f228e65bf9beaa716d188b260d61842de
Andris <laczkovicsandris07.education@gmail.com>
2025-07-28 16:52:04 +0200

    Kisebb CSS finomhangolások

---
+++ 2024-2025/Webprogramozás/Gyakorlat/JavaScript/feladat_js_alapok/feladat01.js
@@ -0,0 +1,63 @@
+"use strict";
+
+// 2. feladat
+console.log(`2. feladat: Hello World!`);
+
+// 3. feladat
+let familyName = "Laczkovics";
+
+console.log(`3. a feladat: ${familyName}`);
+console.log(`3. b feladat: ${typeof(familyName)}`);
+
+// 4. feladat
+let givenName = "András Gergő";
+
+// 5. feladat
+let fullName = familyName + " " + givenName;
+
+console.log(`5. feladat: ${fullName}`);
+
+// 6. feladat
+let a = 10;
+let b = 10;
+
+let kiirasErtek;
+if (a == b)
+{
+    kiirasErtek = "A két változó értéke egyezik";
+}
+else
+{
+    kiirasErtek = "A két változó értéke nem egyezik";
+}
+console.log(`6. a feladat: ${kiirasErtek}`);
+
+let kiirasErtekTipus;
+if (a === b)
+{
+    kiirasErtekTipus = "A két változó értéke és típusa egyezik";
+}
+else
+{
+    kiirasErtekTipus = "A két változó értéke és típusa nem egyezik";
+}
+console.log(`6. b feladat: ${kiirasErtekTipus}`);
+
+// 7. feladat
+console.log(`7. a feladat: ${a + b}`);
+console.log(`7. b feladat: ${a - b}`);
+console.log(`7. c feladat: ${a * b}`);
+console.log(`7. d feladat: ${a / b}`);
+
+// 8. feladat
+let c = 100;
+
+console.log(`8. a feladat: ${(a + c) - b}`);
+
+// 9. feladat
+console.log(`9. feladat: `);
+
+for (let i = 1; i <= 10; i++)
+{
+    console.log(i);
+}
