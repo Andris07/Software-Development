3efc674b060d9fe96b22c8789d8e99299aff25d3
Andris <laczkovicsandris07.education@gmail.com>
2026-05-12 15:46:07 +0200

    Asztali Celeb feladat

---
+++ 2025-2026/Asztali alkalmazások fejlesztése/Gyakorlat/Celeb_feladat/Celeb/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Celeb_LIB;
+
+namespace Celeb
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Ember> emberek = new List<Ember>();
+
+        void Feltoltes()
+        {
+            foreach (var fajlSor in File.ReadAllLines("hires.txt").Skip(1))
+            {
+                emberek.Add(new Ember(fajlSor));
+            }
+        }
+
+        void Nemzetisegek()
+        {
+            var nemzetisegek = emberek.Select(x => x.Nemzetiseg).Distinct().Order();
+            cb_Nemzetiseg.Items.Clear();
+            cb_Nemzetiseg.ItemsSource = nemzetisegek;
+            cb_Nemzetiseg.SelectedIndex = 0;
+        }
+
+        void VisszaAllitas()
+        {
+            tb_Nev.Text = string.Empty;
+            cb_Foglalkozas.SelectedIndex = 0;
+            cb_Nemzetiseg.SelectedIndex = 0;
+            ch_Vilaghiru.IsChecked = false;
+            rb_Ferfi.IsChecked = true;
+            rb_No.IsChecked = false;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Feltoltes();
+            Nemzetisegek();
+        }
+
+        private void bt_Rogzit_Click(object sender, RoutedEventArgs e)
+        {
+            if (tb_Nev.Text == string.Empty)
+            {
+                MessageBox.Show("Adja meg a híres ember nevét!", "Hiba!");
+            }
+
+            // név;foglalkozás;nemzetiség;világhírű;nem
+            // Török Ferenc;sportoló;magyar;igen;férfi
+            string urlapAdat = $"{tb_Nev.Text};{cb_Foglalkozas.Text};{cb_Nemzetiseg.Text};{(ch_Vilaghiru.IsChecked == true ? "igen" : "nem")};{(rb_Ferfi.IsChecked == true ? "férfi" : "nő")}";
+            File.AppendAllText("hires.txt", urlapAdat, Encoding.UTF8);
+
+            VisszaAllitas();
+        }
+
+        private void bt_Megsem_Click(object sender, RoutedEventArgs e)
+        {
+            VisszaAllitas();
+        }
+    }
+}
---
+++ 2025-2026/Asztali alkalmazások fejlesztése/Gyakorlat/Celeb_feladat/Celeb_LIB/Ember.cs
@@ -0,0 +1,22 @@
+﻿namespace Celeb_LIB
+{
+    public class Ember
+    {
+        // név;foglalkozás;nemzetiség;világhírű;nem
+        public string Nev { get; init; }
+        public string Foglalkozas { get; init; }
+        public string Nemzetiseg { get; init; }
+        public string Vilaghiru { get; init; }
+        public string Nem { get; init; }
+
+        public Ember(string fajlSor)
+        {
+            string[] adatok = fajlSor.Split(";");
+            this.Nev = adatok[0];
+            this.Foglalkozas = adatok[1];
+            this.Nemzetiseg = adatok[2];
+            this.Vilaghiru = adatok[3];
+            this.Nem = adatok[4];
+        }
+    }
+}
