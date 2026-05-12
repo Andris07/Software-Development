ea126aaf28951ec73b619ac4f4deb7339f76fe70
Andris <laczkovicsandris07.education@gmail.com>
2026-05-12 15:48:20 +0200

    Asztali Labirintus feladat

---
+++ 2025-2026/Asztali alkalmazások fejlesztése/Gyakorlat/Labirintus_feladat/Labirintus/Program.cs
@@ -0,0 +1,16 @@
+﻿using Labirintus_lib;
+
+Console.WriteLine("5. feladat");
+LabSim lab = new LabSim("Lab1.txt");
+Console.WriteLine("Labirintus betöltve.\n");
+
+Console.WriteLine("6. feladat - Labirintus megjelenítése:");
+lab.KiirLab();
+
+Console.WriteLine("\n8. feladat - Útkeresés szimuláció:");
+lab.Utkereses(szimulacio: true);
+
+Console.WriteLine("\nÚtkeresés végeredménye:");
+lab.KiirLab();
+
+Console.WriteLine("\nProgram vége.");
---
+++ 2025-2026/Asztali alkalmazások fejlesztése/Gyakorlat/Labirintus_feladat/Labirintus_lib/LabSim.cs
@@ -0,0 +1,139 @@
+﻿namespace Labirintus_lib
+{
+    public class LabSim
+    {
+        private List<string> Adatsorok;
+        private char[,] Lab;
+
+        private int SorokSzama;
+        private int OszlopokSzama;
+        private int KijaratSorIndex;
+        private int KijaratOszlopIndex;
+
+        public LabSim(string forrasAllomany)
+        {
+            this.Adatsorok = new List<string>();
+            BeolvasAdatsorok(forrasAllomany);
+            MeretMeghatarozas();
+
+            this.Lab = new char[SorokSzama, OszlopokSzama];
+            FeltoltLab();
+            KijaratIndexekMeghatarozasa();
+        }
+
+        private void BeolvasAdatsorok(string forrasAllomany)
+        {
+            using (StreamReader fajlSorok = new StreamReader(forrasAllomany))
+            {
+                string fajlSor;
+
+                while ((fajlSor = fajlSorok.ReadLine()) != null)
+                {
+                    Adatsorok.Add(fajlSor);
+                }
+            }
+        }
+
+        private void MeretMeghatarozas()
+        {
+            SorokSzama = Adatsorok.Count();
+            OszlopokSzama = 0;
+
+            foreach (string sor in Adatsorok)
+            {
+                if (sor.Length > OszlopokSzama)
+                    this.OszlopokSzama = sor.Length;
+            }
+        }
+
+        private void FeltoltLab()
+        {
+            for (int i = 0; i < SorokSzama; i++)
+            {
+                string sor = Adatsorok[i];
+
+                for (int j = 0; j < OszlopokSzama; j++)
+                {
+                    if (j < sor.Length)
+                        Lab[i, j] = sor[j];
+                    else
+                        Lab[i, j] = ' ';
+                }
+            }
+        }
+
+        private void KijaratIndexekMeghatarozasa()
+        {
+            KijaratSorIndex = SorokSzama - 2;
+            KijaratOszlopIndex = OszlopokSzama - 1;
+        }
+
+        public void KiirLab()
+        {
+            for (int i = 0; i < SorokSzama; i++)
+            {
+                for (int j = 0; j < OszlopokSzama; j++)
+                {
+                    Console.Write(Lab[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void Utkereses(bool szimulacio = false)
+        {
+            bool KeresesKesz = false;
+            bool NincsMegoldas = false;
+            int r = 1;
+            int c = 0;
+
+            while (!KeresesKesz && !NincsMegoldas)
+            {
+                if (Lab[r, c] != '0')
+                    Lab[r, c] = '0';
+
+                if (c + 1 < OszlopokSzama && Lab[r, c + 1] == ' ')
+                {
+                    c++;
+                }
+                else if (r + 1 < SorokSzama && Lab[r + 1, c] == ' ')
+                {
+                    r++;
+                }
+                else
+                {
+                    Lab[r, c] = '-';
+
+                    if (c - 1 >= 0 && Lab[r, c - 1] == '0')
+                    {
+                        c--;
+                    }
+                    else if (r - 1 >= 0 && Lab[r - 1, c] == '0')
+                    {
+                        r--;
+                    }
+                }
+
+                KeresesKesz = (r == KijaratSorIndex && c == KijaratOszlopIndex);
+
+                if (KeresesKesz)
+                    Lab[r, c] = '0';
+
+                NincsMegoldas = (r == 1 && c == 0 && Lab[r, c] == '-');
+
+                if (szimulacio)
+                {
+                    Console.Clear();
+                    Console.WriteLine("8. feladat - Szimuláció");
+                    KiirLab();
+                    Thread.Sleep(300);
+                }
+            }
+        }
+
+        public int GetSorokSzama() => SorokSzama;
+        public int GetOszlopokSzama() => OszlopokSzama;
+        public int GetKijaratSorIndex() => KijaratSorIndex;
+        public int GetKijaratOszlopIndex() => KijaratOszlopIndex;
+    }
+}
