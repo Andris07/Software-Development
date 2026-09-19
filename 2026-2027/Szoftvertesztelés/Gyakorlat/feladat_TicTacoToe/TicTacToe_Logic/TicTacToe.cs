898b9d3e6e13b1c89259cb8bc1451f0cfb5b7cb1
Andris <laczkovicsandris07.education@gmail.com>
2026-09-19 20:13:26 +0200

    Szoftvertesztelés 09.15 órai (TicTacToe)

---
+++ 2026-2027/Szoftvertesztelés/Gyakorlat/feladat_TicTacoToe/TicTacToe_Logic/TicTacToe.cs
@@ -0,0 +1,68 @@
+﻿using System.Data;
+using System.Reflection.Metadata.Ecma335;
+
+namespace TicTacToe_Lib;
+
+public class TicTacToe
+{
+    private int[,] gameMap;
+    public int actualPlayer;
+
+    public TicTacToe()
+    {
+        gameMap = new int[3,3];
+        actualPlayer = 1;
+    }
+
+    public bool isEmpty(int x, int y)
+    {
+        return gameMap[x, y] == 0;
+    }
+
+    public bool Place(int x,int y)
+    {
+        if (GameWon()) throw new Exception("megnyert játék");
+        if (NoMoreMoves()) throw new Exception("nincs több lépés");
+
+        if (isEmpty(x, y))
+        // && !GameWon() && !NoMoreMoves()
+        {
+            gameMap[x, y] = actualPlayer;
+            if (actualPlayer == 1) actualPlayer++;
+            else actualPlayer = 1;
+            return true;
+        }
+        return false;
+    }
+
+    public bool NoMoreMoves()
+    {
+        foreach(int field in gameMap)
+        {
+            if (field == 0) return false; // we found an empty space, so we return false (meaning it is not true, that there are no more moves
+        }
+        return true;
+    }
+
+    public bool GameWon()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            // check horizontal and vertical
+            if ((gameMap[0, i]> 0 && gameMap[0, i] == gameMap[1, i] && gameMap[0, i] == gameMap[2, i]) ||
+                // the field is not empty and all field in the row are the same
+               (gameMap[i, 0] > 0 && gameMap[i, 0] == gameMap[i, 1] && gameMap[i, 0] == gameMap[i, 0]))
+            {   // the field is not empty and all field in the column are the same
+                return true;
+            }
+        }
+
+        // check diagonals
+            if ((gameMap[0, 0]> 0 && gameMap[0, 0] == gameMap[1, 1] && gameMap[0, 0] == gameMap[2, 2]) ||  
+               (gameMap[0, 2] > 0 && gameMap[0, 2] == gameMap[1, 1] && gameMap[0, 2] == gameMap[2, 0]))
+            {   // the field is not empty and all field in the diagonal are the same
+                return true;
+            }
+        return false;
+    }
+}
---
+++ 2026-2027/Szoftvertesztelés/Gyakorlat/feladat_TicTacoToe/TicTacoToe/TicTacToeUnitTest.cs
@@ -0,0 +1,71 @@
+﻿using TicTacToe_Lib;
+namespace TicTacoToe_Test
+
+{
+    public class Tests
+    {
+        TicTacToe game;
+
+        [SetUp]
+        public void Setup()
+        {
+            game = new();
+        }
+
+        [Test]
+        public void TestCanPlaceOnEmptyField()
+        {
+            Assert.That(game.isEmpty(0, 0), Is.True, "By default, the field should be empty");
+            Assert.That(game.Place(0, 0), Is.True, "Should be able to place on empty space");
+
+        }
+        [Test]
+        public void TestCanNotPlaceOnTakenField()
+        {
+            Assert.That(game.isEmpty(0, 0), Is.True, "By default, the field should be empty");
+            Assert.That(game.Place(0, 0), Is.True, "Should be able to place on empty space");
+            Assert.That(game.Place(0, 0), Is.False, "Can't place on taken field");
+        }
+        [Test]
+        public void TestPlayerTurnChange()
+        {
+            Assert.That(game.actualPlayer, Is.EqualTo(1), "At game start, actual player should be #1");
+            Assert.That(game.Place(0, 0), Is.True, "Should be able to place on empty space");
+            Assert.That(game.actualPlayer, Is.EqualTo(2), "After placing, the player should change from #1 to #2");
+            Assert.That(game.Place(1, 0), Is.True, "Should be able to place on other empty space");
+            Assert.That(game.actualPlayer, Is.EqualTo(1), "After placing, the player should change from #2 to #1");
+        }
+        [Test]
+        public void TestNoMoreMove()
+        {
+            Assert.That(game.NoMoreMoves(), Is.False, "at the game start, there should be more possible moves");
+            Assert.That(game.Place(0, 0), Is.True, "place first");
+            Assert.That(game.Place(1, 0), Is.True, "place second");
+            Assert.That(game.Place(2, 0), Is.True, "place third");
+            Assert.That(game.NoMoreMoves(), Is.False, "after a few turns, there should be more possible moves");
+            Assert.That(game.Place(0, 1), Is.True);
+            Assert.That(game.Place(1, 1), Is.True);
+            Assert.That(game.Place(2, 1), Is.True);
+            Assert.That(game.Place(1, 2), Is.True);
+            Assert.That(game.Place(0, 2), Is.True);
+            Assert.That(game.Place(2, 2), Is.True);
+            Assert.That(game.NoMoreMoves(), Is.True, "after the board is full and there is no winner, there should be no more moves");
+        }
+        [Test]
+        public void TestGameWon()
+        {
+            Assert.That(game.NoMoreMoves(), Is.False, "at the game start, there should be more possible moves");
+            Assert.That(game.GameWon(), Is.False, "at the game start, the should not be in won state");
+            Assert.That(game.Place(0, 0), Is.True);
+            Assert.That(game.Place(1, 0), Is.True);
+            Assert.That(game.Place(2, 0), Is.True);
+            Assert.That(game.NoMoreMoves(), Is.False, "after a few turns, there should be more possible moves");
+            Assert.That(game.Place(0, 1), Is.True);
+            Assert.That(game.Place(1, 1), Is.True);
+            Assert.That(game.Place(2, 1), Is.True);
+            Assert.That(game.Place(0, 2), Is.True);
+            Assert.That(game.GameWon(), Is.True, "Last move was a winning move, Game should be in a Won State");
+            Assert.That(()=>game.Place(1, 2) , Throws.Exception,"Game is won, no more moves allowed");
+        }
+    }
+}
